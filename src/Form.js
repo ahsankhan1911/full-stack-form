4e98bc3565b70a0201885cd011bc35f5ff1a4768
Ahsan Khan <ahsankhan1911@gmail.com>
2017-08-30 15:15:08 +0500

    workkkkkk

--- src/Form.js
+++ src/Form.js
@@ -1,74 +1,40 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-
 import axios from 'axios';
 
-
+let saad 
 @inject('User')
 @observer
 class Form extends Component {
 
     onSubmit = (e) => {
+        e.preventDefault();
         let { User } = this.props;
+
         User.firstname = this.refs.firstname.value
         User.lastname = this.refs.lastname.value
         User.email = this.refs.email.value
         User.phone = this.refs.phone.value
         User.password = this.refs.password.value
 
+        User.createUser();
+    }
 
 
+    onSubmitShow = (e) =>  {
+        let { User } = this.props;
         e.preventDefault();
-        axios.post('http://localhost:3030/user/create-user', {
+    
+       User.showUser();
 
-
-            firstname: User.firstname,
-            lastname: User.lastname,
-            email: User.email,
-            phone: User.phone,
-            password: User.password,
-        })
-
-            .then(function (response) {
-
-                let Err = response.data.errors
-
-                if (Err.firstname) {
-                    alert(Err.firstname.message)
-                }
-
-                if (Err.lastname) {
-                    alert(Err.lastname.message)
-                }
-
-                if (Err.password) {
-                    alert(Err.password.message)
-                }
-
-                if (Err.email) {
-                    alert(Err.email.message)
-                }
-
-                if (Err.phone) {
-                    return alert(Err.phone.message)
-                }
-
-              
-
-                alert('Thanks for signing up ' + response.data.firstname)
-
-
-
-
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-
-
+       console.log(User.userData);
+  
+         
+      
     }
 
     render() {
+        let {User} = this.props;
         return (
             <div className="container-fluid">
                 <h1> &nbsp;&nbsp;Full Stack Form</h1><br />
@@ -78,8 +44,16 @@ class Form extends Component {
                     <input className="form-control" name="email" placeholder="Email" ref="email" /><br />
                     <input className="form-control" name="phone" placeholder="Phone" ref="phone" /><br />
                     <input className="form-control" name="password" type="password" placeholder="password" ref="password" /><br />
-                    <button className="btn btn-primary" onClick={e => this.onSubmit(e)}>submit</button>
+                    <button className="btn btn-primary" onClick={e => this.onSubmit(e)}>submit</button>&nbsp;&nbsp;
+                    <button className="btn btn-primary" onClick={e => this.onSubmitShow(e)}>show all users</button>
                 </form>
+                <h1> Users Data</h1>
+            
+                    
+                        <div>{saad}</div>
+                  
+                
+                
             </div>
         )
     }
--- src/stores/Appstate.js
+++ src/stores/Appstate.js
@@ -1,27 +1,82 @@
 import { observable, computed, action } from 'mobx';
+import axios from 'axios';
 
-// class User {
-//     @observable firstname   
-//     @observable lastname 
-//     @observable email
-//     @observable phone
-//     @observable password
 
-//     constructor(firstname , lastname, email,phone, password ){
-//         this.firstname = firstname
-//         this.lastname = lastname
-//         this.email = email
-//         this.phone = phone
-//         this.password = password
-//     }
-// }
+let user;
 
-class Users {   
+class Users {
     @observable firstname = '';
     @observable lastname = '';
     @observable email = '';
     @observable phone = '';
     @observable password = '';
+    @observable userData = [];    
+
+    @action createUser() {
+
+        axios.post('http://localhost:5000/users/create-user', {
+
+
+            firstname: this.firstname,
+            lastname: this.lastname,
+            email: this.email,
+            phone: this.phone,
+            password: this.password,
+        })
+
+            .then(function (response) {
+
+                let Err = response.data.errors
+                if (Err) {
+                    if (Err.firstname) {
+                        alert(Err.firstname.message)
+                    }
+
+                    if (Err.lastname) {
+                        alert(Err.lastname.message)
+                    }
+
+                    if (Err.password) {
+                        alert(Err.password.message)
+                    }
+
+                    if (Err.email) {
+                        alert(Err.email.message)
+                    }
+
+                    if (Err.phone) {
+                        return alert(Err.phone.message)
+                    }
+
+                }
+                alert('Thanks for signing up ' + response.data.firstname)
+
+
+
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+
+    }
+
+
+    @action showUser() {
+        let user = this.userData;
+     axios.get('http://localhost:5000/users/show-user')
+        .then(function (response) {
+            
+            response.data.map(data => {
+                     user.push(data);
+            } )
+    
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
 
 
 };
